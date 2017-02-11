c89284c254f7728655a49d608c1f03532b1f72df
rinngo <rinngo_381167381167@yahoo.co.jp>
2017-02-12 04:40:42 +0900

    Add files via upload

---
+++ kadai/app.rb
@@ -0,0 +1,35 @@
+﻿#coding: utf-8
+Encoding.default_external='utf-8'
+# ライブラリのロード
+require 'sequel'
+require 'sinatra'
+
+# DBへの接続
+db_path = File.dirname(__FILE__) + "/keijiban.db"
+DB = Sequel.sqlite(db_path)
+
+#DB内にcommentsテーブルがあるか判断
+if !(DB.table_exists?(:comments)) then
+  #テーブルがない場合作成
+  DB.create_table :comments do
+    primary_key :id
+    String :name
+    String :comment
+  end
+end
+
+class Comments<Sequel::Model
+end
+
+#掲示板の内容表示
+get '/' do
+  @comments=Comments.all
+  erb :layoutt
+end
+
+#コメントの追加・表示
+post '/' do
+  Comments.insert(:name=>params[:name],:comment=>params[:comment])
+  @comments=Comments.all
+  erb :layoutt
+end
